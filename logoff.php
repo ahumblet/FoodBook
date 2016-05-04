3ef4453c446df3bf8b6c183f0d66d6e99ff84011
ahumblet <amhumblet@gmail.com>
2016-05-03 23:51:25 -0400

    progress on newProfile page.

---
+++ logoff.php
@@ -0,0 +1,59 @@
+<?php	
+	//define a global javascript error variable
+	if ($_GET["userError"] == 1) {
+		echo '<script type="text/javascript">', 'var userError = 1;', '</script>';
+	} else if ($_GET["userError"] == 2) {
+		echo '<script type="text/javascript">', 'var userError = 2;', '</script>';
+	}
+	
+	echo <<< EOT
+	<html>
+		<head>
+			<link rel="stylesheet" href="login.css">
+		</head>
+	
+		<body>
+			<div class="login-page">
+				<div class="form">
+					<!-- java script to toggle forms -->
+					<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+					<script>
+					$(document).ready(function(){
+						$("p").click(function(){
+							$('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+						});
+					});
+					</script>
+	
+					<script type="text/javascript">
+					if (userError == 1) {
+						document.write('<p class="message">Invalid username and/or password. Try again</p><br>');
+					} else if (userError == 2) {
+						document.write('<p class="message">Username already exists. Try again</p><br>');
+					}
+					</script>
+
+					<form class="login-form" action="checkLogin.php" method="post">
+						<input type="text" name="username" placeholder="username"/>
+						<input type="password" name="password" placeholder="password"/>
+						<button name="login" type="submit">Login</button>
+						<p class="message">Not registered? <a href="#">Create an account</a></p>
+					</form>
+					<!-- register form -->
+					<form class="register-form" action="checkLogin.php" method="post">
+						<input type="text" name="username" placeholder="name"/>
+						<input type="password" name="password" placeholder="password"/>
+						<input type="text" name="email" placeholder="email address"/>
+						<button name="register" type="submit">Register</button>
+						<p class="message">Already registered? <a href="#" onclick="function()"> Sign In</a></p>
+					</form>
+				</div>
+			</div>
+		</body>
+	</html>
+EOT;
+	
+?>
+
+
+
--- newProfile.php
+++ newProfile.php
@@ -2,9 +2,45 @@
 	
 	session_start();
 	$loggedInUser = $_SESSION["loggedInUser"];
+	$profileUsername = $_GET["username"];
 	
 	include_once 'externalFunctions.php';
 	startMysqli();
+	
+	//get the profile entry
+	$query = sprintf("select * from profile natural join user where profile.username ='%s'", $profileUsername);
+	$result = $mysqli->query($query);
+	
+	if ($result->num_rows > 0) {
+		$row = $result->fetch_assoc();
+		//check visibility
+		$visibility = $row["visibility"];
+		$permission = hasPermission($loggedInUser, $profileUsername, $visibility);
+		$mysqli->kill();
+		$mysqli = new mysqli("$localhost", "$user", "$password", "$db");
+		if ($permission != True) {
+			printf("You do not have permission to view %s's profile", $profileUsername);
+		} else {
+			//get all the info for the profile
+			$photo = $row["photo"];
+			$firstName = $row["firstName"];
+			$lastName = $row["lastName"];
+			$age = $row["age"];
+			$email = $row["email"];
+			$type = $row["type"];
+			//get liked locations
+			$query = sprintf("select * from interactiveLike join location where interactiveLike.likedInteractiveID = location.interactiveID and likingUser = '%s'", $profileUsername);
+			$result = $mysqli->query($query);
+			$likedLocations = array('atleastOne');
+			if ($result->num_rows > 0) {
+				while ($row = $result->fetch_assoc()) {
+					$location = $row["locName"];
+					$interactiveID = $row["interactiveID"];
+					array_push($likedLocations, $location);
+				}
+			}
+		}
+	}
 	
 echo <<< EOT
 <html>
@@ -22,12 +58,12 @@ echo <<< EOT
 				</div>
 		</div>
 		<ul class="menu">
-			<a href="login.php"> <li><i class="fa fa-user"></i>Profile</li> </a>
-			<a href="login.php"> <li><i class="fa fa-home"></i>Wall</li> </a>
-			<a href="login.php"> <li><i class="fa fa-home"></i>Feed</li> </a>
-			<a href="login.php"> <li class="active"><i class="fa fa-group"></i>Friends</li> </a>
-			<a href="login.php"> <li><i class="fa fa-envelope"></i>Locations</li> </a>
-			<a href="login.php"> <li><i class="fa fa-cog"></i>Log Out</li> </a>
+			<a href="profile.php"> <li class="active"><i class="fa fa-user"></i>Profile</li> </a>
+			<a href="wall.php"> <li><i class="fa fa-home"></i>Wall</li> </a>
+			<a href="feed.php"> <li><i class="fa fa-home"></i>Feed</li> </a>
+			<a href="friends.php"> <li><i class="fa fa-group"></i>Friends</li> </a>
+			<a href="locations.php"> <li><i class="fa fa-envelope"></i>Locations</li> </a>
+			<a href="logoff.php"> <li><i class="fa fa-cog"></i>Log Out</li> </a>
 		</ul>
 	</nav>
 		<div class="header">
@@ -35,14 +71,14 @@ echo <<< EOT
 		<div class="pic"></div>
 EOT;
 	
-printf('<div class="welcome"><span>Welcome</span><span class="name">%s!</span></div></div>', $loggedInUser);
+printf('<div class="welcome">Welcome %s!</div></div>', $loggedInUser);
 	
 echo <<< EOT
 	<div class="title"></div>
 	<div class="filler">
 EOT;
-	
-printf("This should be after the header.This should be after the header.This should be after the header.This should be after the header.This should be after the header.This should be after the header.This should be after the header.This should be after the header.This should be after the header.This should be after the header.This should be after the header.This should be after the header.");
+
+	generateProfileHTML();
 	
 echo <<< EOT
 	</div>
@@ -52,4 +88,37 @@ echo <<< EOT
 	</html>
 EOT;
 	
-	?>
+
+function generateProfileHTML() {
+	global $photo, $firstName, $lastName, $age, $email, $type, $loggedInUser, $profileUsername, $likedLocations;
+	
+	if ($photo != 'NULL') {
+		printf('<div class="photo">');
+		echo '<img src="data:image/jpeg;base64, ' . base64_encode($photo) . '" height="350" width="350" align="right"/> <br>';
+		printf("</div>");
+	}
+	printf("First Name: %s <br>", $firstName);
+	printf("Last Name: %s <br>", $lastName);
+	printf("Age: %s <br>", $age);
+	printf("Email: %s <br>", $email);
+	printf("Type: %s <br>", $type);
+	printf("Liked Locations: ");
+	foreach ($likedLocations as &$location) {
+		printf("one%s, ", $location);
+	}
+	printf("<br><br>");
+	//edit profile button
+	if ($loggedInUser == $profileUsername) {
+	echo '<form action="editProfile.php">';
+	echo '<input type="submit" value="Edit Profile">';
+	echo '</form>';
+	}
+	
+	 //link to wall
+	 printf("<br><br><a href='http://localhost:8888/finalProject/wall.php?username=%s'>View %s's wall</a>", $profileUsername, $profileUsername);
+}
+	
+	
+	
+	
+?>

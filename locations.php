3db387c355b6adeb87b7ed0b83851806f14f0499
ahumblet <amhumblet@gmail.com>
2016-05-04 01:32:54 -0400

    implemented logoff and integrated css into locations page

--- locations.php
+++ locations.php
@@ -2,35 +2,57 @@
 	session_start();
 	$loggedInUser = $_SESSION["loggedInUser"];
 	
-	echo "<link rel='stylesheet' href='login.css'>";
-	
 	include_once 'externalFunctions.php';
 	startMysqli();
 	
-	printf("Locations:<br><br>");
-	
-	//form to create new location
-	printf('<form action="addOrLikeLocation.php" method="post">');
-	printf('Name: <input type="text" name="locName"><br>');
-	printf('Longitude: <input type="text" name="longitude"><br>');
-	printf('Latitude: <input type="text" name="latitude"><br>');
-	printf('<input type="submit" value="Add Location" name="addLocation"> <br>');
-	printf('</form><br>');
-	
-	//show locations with like buttons
+	//get locations
 	$query = sprintf("select * from location");
 	$result = $mysqli->query($query);
-	printf('<form action="addOrLikeLocation.php" method="post">');
+	$locations = array();
+	$locationIds = array();
 	if ($result->num_rows > 0) {
 		while($row = $result->fetch_assoc()) {
-			printf("locname = %s, interactiveID = %s, &nbsp&nbsp", $row["locName"], $row["interactiveID"]);
-			printf('<button name="likeLocation" value="%s" type="submit">Like</button><br>', $row["interactiveID"]);
+			$locName = $row["locName"];
+			$locationId = $row["interactiveID"];
+			array_push($locations, $locName);
+			array_push($locationIds, $locationId);
 		}
 	}
-	printf('</form>');
 	
+	generateHTMLTop('locations');
+	generateLocationsPage();
+	generateHTMLBottom();
 	
-	printf('<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.nz/maps?q=120+Mayoral+Dr,+Auckland+University+of+Technology+-+City+Campus,+Auckland,+1010&amp;ie=UTF8&amp;hq=&amp;hnear=120+Mayoral+Dr,+Auckland,+1010&amp;t=m&amp;z=13&amp;ll=-36.859383,174.777836&amp;output=embed"></iframe><br />');
+	//=============== Function Definitions ============//
+	
+	function generateLocationsPage() {
+		global $loggedInUser, $locations, $locationIds;
+		
+		printf("Locations:<br><br>");
+		
+		//form to create new location
+		printf('<form action="addOrLikeLocation.php" method="post">');
+		printf('Name: <input type="text" name="locName"><br>');
+		printf('Longitude: <input type="text" name="longitude"><br>');
+		printf('Latitude: <input type="text" name="latitude"><br>');
+		printf('<input type="submit" value="Add Location" name="addLocation"> <br>');
+		printf('</form><br>');
+		
+		//show locations with like buttons
+		printf('<form action="addOrLikeLocation.php" method="post">');
+		for ($i = 0; $i < count($locations); ++$i) {
+			$location = $locations[$i];
+			$locationId = $locationIds[$i];
+			printf("%s", $location);
+			printf('<button name="likeLocation" value="%s" type="submit">Like</button><br>', $locationId);
+			
+		}
+		printf('</form>');
+		
+		//google map
+		printf('<iframe width="425" height="350" align="right" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.nz/maps?q=120+Mayoral+Dr,+Auckland+University+of+Technology+-+City+Campus,+Auckland,+1010&amp;ie=UTF8&amp;hq=&amp;hnear=120+Mayoral+Dr,+Auckland,+1010&amp;t=m&amp;z=13&amp;ll=-36.859383,174.777836&amp;output=embed"></iframe><br />');
+	}
+	
 ?>
 
 
